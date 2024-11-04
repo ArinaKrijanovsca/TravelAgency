971dce5b5397e936f35f8fec97693b406ffff201
arina-raya <raya_1705_5@mail.ru>
2024-11-04 14:37:33 +0200

    add

---
+++ TravelAgencyKrishanovskaya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelAgencyKrishanovskaya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
